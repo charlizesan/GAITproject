c18a88ebfd82ad6a8f89a4eb461d8974056194c2
Charlize Santos <142826095+charlizesan@users.noreply.github.com>
2023-12-12 00:28:44 -0600

    Finished product

--- project.py
+++ project.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from gtts import gTTS
+import os
 
 app = Flask(__name__)
 
@@ -6,6 +8,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route('/video', methods=['GET', 'POST'])
+def video():
+    text_to_speech_text = ""
+    video_filename = request.form.get('selected_video', 'default.mp4')  # Get the selected video filename
 
+    if request.method == 'POST':
+        text_to_speech_text = request.form.get('text', '')
+        selected_language = request.form.get('language', 'en')
+
+        if text_to_speech_text:
+            tts = gTTS(text=text_to_speech_text, lang=selected_language)
+            tts.save('static/speech.mp3')
+
+    return render_template('video.html', text_to_speech_text=text_to_speech_text, video_filename=video_filename)
+
+
+
+@app.route('/speech/<filename>')
+def speech(filename):
+    return send_from_directory('static', filename)
+
+if __name__ == '__main__':
     app.run(debug=True)
